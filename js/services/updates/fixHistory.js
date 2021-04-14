498af31c8dc18d195c40b4759635ceb26d9eb989
Riku Avelar <riku.avelar@gmail.com>
2021-04-14 11:29:51 -0400

    Add missing stringify to history fix

--- js/services/updates/fixHistory.js
+++ js/services/updates/fixHistory.js
@@ -1,12 +1,17 @@
 export const fixHistory = () => {
-    const history = JSON.parse(localStorage.getItem('history'));
-
-    if (!history || !Array.isArray(history)) {
-        return;
+    try {
+        const history = JSON.parse(localStorage.getItem('history'));
+    
+        if (!history || !Array.isArray(history)) {
+            return;
+        }
+    
+        localStorage.setItem('history', JSON.stringify(history.map(enc => ({
+            ...enc,
+            combatants: enc.combatants.map(c => ({...c, dps: Math.floor(c.dps)}))
+        }))))
+    } catch (e) {
+        // History broke, unfortunately needs to be reset
+        localStorage.setItem('history', '[]')
     }
-
-    localStorage.setItem('history', history.map(enc => ({
-        ...enc,
-        combatants: enc.combatants.map(c => ({...c, dps: Math.floor(c.dps)}))
-    })))
 }
